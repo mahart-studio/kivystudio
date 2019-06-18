e7a59a3122fcb2e8f96eda2d22b13bf366533794
avour <avour123@gmail.com>
2019-06-18 18:45:17 +0100

    made topmenu file drop down fuctions

--- kivystudio/widgets/dropdown.py
+++ kivystudio/widgets/dropdown.py
@@ -1,6 +1,6 @@
 from kivystudio.behaviors import HoverBehavior
 from kivy.uix.dropdown import DropDown
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, BooleanProperty
 from kivy.lang import Builder
 
 
@@ -22,9 +22,19 @@ Builder.load_string('''
 class DropDownBase(HoverBehavior, DropDown):
     
     is_open = ObjectProperty(False)
+    
+    auto_touch_dismiss = BooleanProperty(True)
 
     def on_open(self):
         self.is_open = True
 
     def on_dismiss(self):
         self.is_open = False
+
+    def on_touch_up(self, touch):
+        if self.collide_point(*touch.pos):
+            if self.auto_touch_dismiss:
+                self.dismiss()
+        
+        return super(DropDownBase,self).on_touch_up(touch)
+
