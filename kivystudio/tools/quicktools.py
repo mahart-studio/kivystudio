1902181492eddd35026dc2bc113189acea78177c
avour <avour123@gmail.com>
2019-07-10 02:43:08 +0100

    quicktool funct to detect binary files

--- kivystudio/tools/quicktools.py
+++ kivystudio/tools/quicktools.py
@@ -35,3 +35,23 @@ def save_as():
 
 def exit_window():
     exit()
+
+import string
+def is_binary(filename):
+    s = open(filename).read(512)
+    text_char = ''.join( list(map(chr, range(32,127))) + list('\n\r\t\b') )
+    _null_trans = string.maketrans('', '')
+
+    if not s: # empty files are considered text files
+        return False
+    
+    if '\0' in s:
+        # file with null bytes are likely binary
+        return True
+
+    t = s.translate(_null_trans, text_char)
+    # if more than 30% are non-text charaters 
+    # then it is considered binary
+    if float(len(t))/float(len(s)) > 0.30:
+        return True
+    return False
